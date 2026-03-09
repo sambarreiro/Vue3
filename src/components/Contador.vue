7fdd605538fcfd002c969a7ec440e35e94ac37da
sambarreiro <sambarreiro@gmail.com>
2026-03-09 23:42:08 +0100

    probando primer componente

---
+++ src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ count }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+const decrementar = () => {
+  count.value--;
+};
+const incrementar = () => {
+  count.value++;
+};
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
